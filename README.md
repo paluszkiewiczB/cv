9d25df3fe397beb06c26202779bb3b91d8d0c307
Bartosz Paluszkiewicz <bartosz.paluszkiewicz@docaposte.fr>
2022-03-27 23:32:50 +0200

    implement design from Figma

--- README.md
+++
@@ -1,7 +0,0 @@
-# README
-
-Generate `resume.html` and `resume.pdf` by invoking `asciidoctor-web-pdf` as shown below from the command-line:
-
-```console
-$ asciidoctor-web-pdf resume.adoc --template-require ./template.js
-```
--- template.js
+++ template.js
@@ -1,28 +1,15 @@
 module.exports = {
-  paragraph: (node) => `<p class="paragraph">${node.getContent()}</p>`,
-  document: (node) => `<html lang="fr">
+  document: (node) => `<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>${node.getDocumentTitle()}</title>
-  <link href="https://fonts.googleapis.com/css?family=Bitter|Fira+Sans:100,200,300,400,700" rel="stylesheet">
-  <link rel="stylesheet" href="./style.css">
+  <link href="https://fonts.googleapis.com/css?family=Inter:400,450,600,700" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <div class="page-bleed"></div>
-  <div class="header">
-    <h1>${node.getHeader().getTitle()}</h1>
-  </div>
   <div id="content">
     ${node.getContent()}
   </div>
-  <div class="footer"></div>
-</body>`,
-  section: (node) => {
-        const title = node.getTitle() === '!' ? '' : `<h${node.getLevel()}>${node.getTitle()}</h${node.getLevel()}>`
-        return `<div class="section section${node.getLevel()} ${node.getRole()}">
-  ${title}
-  ${node.getContent()}
-</div>`
-  }
+</body>`
 }
